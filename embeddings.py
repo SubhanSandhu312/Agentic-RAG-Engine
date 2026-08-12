892ae34415c7a24b3528509d6ab35b4f23bee2e6
Subhan Sandhu <subhansandhu312@gmail.com>
2026-08-13 01:21:00 +0500

    Debuigging Faiss

--- embeddings.py
+++ embeddings.py
@@ -2,11 +2,14 @@ from sentence_transformers import SentenceTransformer
 from chunking import chunks
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
+embeddings_list_whole = []
 embeddings_list = []
 for file, content in chunks:
     embeddings = model.encode(content)
-    embeddings_list.append((file, content, embeddings))
+    embeddings_list.append(embeddings)
+    embeddings_list_whole.append((file, content, embeddings))
     print(f"Embeddings for {file}:")
     print(embeddings)
     print("-" * 40)
-print(len(embeddings_list))
+print(len(embeddings_list_whole))
+print(embeddings_list.shape)
